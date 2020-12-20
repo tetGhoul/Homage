f1a0e54fe0ce567f170886c747bff10d7f1769be
tetGhoul <tetghoul@gmail.com>
2020-12-20 00:12:46 -0500

    i fixed some errors so that i can go to sleep

--- Homage2020/Assets/Script/PlayerMovement.cs
+++ Homage2020/Assets/Script/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+
     public float speed = .1f;
     public Animator FemaleAnimator;
 
@@ -20,64 +21,63 @@ public class PlayerMovement : MonoBehaviour
 
 
         transform.position += moveDirection * speed;
-    } 
-}
+    }
 
-        public void PlayerAnimate()
+    public void PlayerAnimate()
+    {
+        if (Input.GetKey(KeyCode.W))
+
         {
-            if (Input.GetKey(KeyCode.W))
-           
-           {
-        FemaleAnimator.SetBool("Jogging"); true;);
-           }
-           if (Input.GetKeyUp(KeyCode.W))
-           {
-    FemaleAnimator.SetBool("Jogging"); false;);
-           }
+            FemaleAnimator.SetBool("Jogging", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            FemaleAnimator.SetBool("Jogging", false);
+        }
 
-if (Input.GetKey(KeyCode.A))
-           
- {
-    FemaleAnimator.SetBool("Jog Strafe Left"); true;);
-}
-if (Input.GetKeyUp(KeyCode.A))
- {
-    FemaleAnimator.SetBool("Jog Strafe Left"); false;);
- }
+        if (Input.GetKey(KeyCode.A))
+
+        {
+            FemaleAnimator.SetBool("Jog Strafe Left", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            FemaleAnimator.SetBool("Jog Strafe Left", false);
+        }
 
 
-
-if (Input.GetKey(KeyCode.S))
-           
-           {
-    FemaleAnimator.SetBool("Backwards Jog"); true;);
-}
-if (Input.GetKeyUp(KeyCode.S))
-{
-    FemaleAnimator.SetBool("Backwards Jog"); false;);
-}
- 
-
-if (Input.GetKey(KeyCode.D))
-
-           {
-    FemaleAnimator.SetBool("Jog Strafe Right"); true;);
-}
-if (Input.GetKeyUp(KeyCode.D))
-{
-    FemaleAnimator.SetBool("Jog Strafe Right"); false;);
-}
-
-if (Input.GetKey(KeyCode.LeftShift))
-
-           {
-    FemaleAnimator.SetBool("Running"); true;);
-}
-if (Input.GetKeyUp(KeyCode.LeftShift))
- {
-    FemaleAnimator.SetBool("Running"); false;);
- }
+        if (Input.GetKey(KeyCode.S))
+        {
+            FemaleAnimator.SetBool("Backwards Jog", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            FemaleAnimator.SetBool("Backwards Jog", false);
+        }
 
 
-}
+        if (Input.GetKey(KeyCode.D))
 
+        {
+            FemaleAnimator.SetBool("Jog Strafe Right", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            FemaleAnimator.SetBool("Jog Strafe Right", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+
+        {
+            FemaleAnimator.SetBool("Running", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            FemaleAnimator.SetBool("Running", false);
+        }
+
+    }
+
+
+    //i cant finsh this fucking code
+    //Jah this shit is up to you 
